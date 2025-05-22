6ccf959054f4c69502a2ef05d4ba72ca80f37355
https://github.com/Mathpatibaswaraj1993/HTMLProject.git <mathpatibaswaraj@gmail.com>
2025-05-22 15:18:02 +0530

    new file added

--- chai_backend/src/models/user.model.js
+++ chai_backend/src/models/user.model.js
@@ -20,7 +20,7 @@ const userSchema = new Schema(
             lowercase: true,
             trim: true
         },
-        fullname: {
+        fullName: {
             type: String,
             required: true,
             trim: true,
@@ -61,6 +61,33 @@ userSchema.pre("save", async function (next) {
 userSchema.methods.isPasswordCorrect = async function
     (password) {
    return await  bcrypt.compare(password,this.password)
+}
+//Generate Access Token 
+userSchema.methods.generateAccessToken = function () {
+   return jwt.sign(
+        {
+            _id: this._id,
+            email: this.email,
+            username: this.username,
+            fullName:this.fullName
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+}
+
+userSchema.methods.generateRefreshToken = function () {
+    return jwt.sign(
+        {
+            _id: this._id,
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN__EXPIRY
+        }
+    )
 }
 
 export const User = mongoose.model("User", userSchema)    
